618b86cb1f52d32b6e9442eef42472a0d826732e
Jason Major <jason.major78@gmail.com>
2022-05-01 16:16:22 -0400

    set the table

--- WasRun.py
+++ WasRun.py
@@ -2,23 +2,40 @@ class TestCase:
   def __init__(self, name):
     self.name = name
 
+  def setUp(self):
+    pass
+
   def run(self):
+    self.setUp()
     method = getattr(self, self.name)
     method()
 
 class WasRun(TestCase):
   def __init__(self, name):
     self.wasRun = None
+    self.wasSetUp = None
     TestCase.__init__(self, name)
 
   def testMethod(self):
     self.wasRun = 1
 
+  def setUp(self):
+    self.wasSetUp = 1
+    self.wasRun = None
+
 class TestCaseTest(TestCase):
+  
+  def setUp(self):
+    self.test = WasRun("testMethod")
+
+  def testSetUp(self):
+    test.run()
+    assert(test.wasSetUp)
+  
   def testRunning(self):
-    test = WasRun("testMethod")
-    assert(not test.wasRun)
     test.run()
     assert(test.wasRun)
 
+
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
